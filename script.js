ac73148f3495701610263b663c2c591a703a1dcc
Karan Meghe <karanmeghe87@gmail.com>
2022-07-11 23:22:42 +0530

    Add transiation class to element

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 const btn = document.querySelector('button');
 const text = document.querySelector('p');
 const box = document.querySelector('.machine-info');
+const machineBox = document.querySelector('#machineId');
 
 const updateBtn = () => {
   if (btn.textContent === 'Start Machine') {
     btn.textContent = 'Stop Machine';
     text.textContent = 'The Machine has Started';
     box.style.backgroundColor = 'Green';
+    machineBox.classList.add('trans');
   } else {
     btn.textContent = 'Start Machine';
     text.textContent = 'The Machine is Stopped';
